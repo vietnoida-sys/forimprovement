dfad652bac6823bb506ddceabebc5d671fa1d485
NKkushwah <nandkishorek717@gmail.com>
2026-07-10 10:48:50 +0530

    changes css fix

--- combined/frontend/src/App.jsx
+++ combined/frontend/src/App.jsx
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from "react"; // 👈 useState aur useEffect import kiya
+import React, { useState, useEffect } from "react"; 
 import AppRoutes from "./routes/AppRoutes";
 import { FaChevronUp } from "react-icons/fa";
 import "./App.css";
 import ScrollToTop from "./components/ScrollToTop";
-import logo from "./assets/vietworldgate.png"; // 👈 Yahan apne logo ka sahi path daal dena
+import logo from "./assets/vietworldgate.png"; 
 import   SwinburneBanner from "./components/SwinburneBanner"
 function App() {
-  const [loading, setLoading] = useState(false); // 👈 Splash screen ke liye state
+  const [loading, setLoading] = useState(false); 
 
   useEffect(() => {
-    // 2 seconds baad loading ko false kar dega
+    
     const timer = setTimeout(() => {
       setLoading(false);
     }, 1000); 
 
-    return () => clearTimeout(timer); // Cleanup timer
+    return () => clearTimeout(timer); 
   }, []);
 
   const scrollToTop = (e) => {
@@ -25,7 +25,7 @@ function App() {
     });
   };
 
-  // Agar loading true hai toh sirf logo dikhega
+  
   if (loading) {
     return (
       <div className="splash-screen">
@@ -34,7 +34,7 @@ function App() {
     );
   }
 
-  // Jaise hi loading false hogi, aapki puri website load ho jayegi
+  
   return (
     <>
       <SwinburneBanner />
