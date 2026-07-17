6571c278e70d6aca1841b4bf0bf1c9613817f8f7
pawan kushwah <pawankushwah23713@gmail.com>
2026-07-17 13:03:07 +0530

    fix it settings

--- combined/backend/routes/Settings.js
+++ combined/backend/routes/Settings.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const ctrl = require("../controllers/settingscontroller");
+const ctrl = require("../controllers/Settingscontroller");
 const { protect, authorize } = require("../middleware/auth");
 
 router.use(protect);
